e3dc1eeef103b8291e0bf49dacc92ef870ff3021
jswagging <jamerchgitcontactmail@yahoo.com>
2025-09-28 01:02:27 -0700

    Update killfeeds.lua

--- killfeeds.lua
+++ killfeeds.lua
@@ -1,6 +1,3 @@
--- # Below, is the entire RS killfeed file, it's a hell, so, like, lock tf in lo
--- # File checked by @CametizedDev
-
 local killMessagePool = {
 	extra.. " played ETOH and immediatelly ragequit",
 	killer.. " sent " ..extra.. " something in the mailbox...",
@@ -8,5 +5,10 @@ local killMessagePool = {
 	killer.. " converted " ..extra.. " into an .svg file",
 	"Dearest " ..killer.. " by the time you're seeing this... I, " ..extra.. " am dead.",
 	extra.. " asked for a game for free from the owner because they had 50 subscribers",
-	killer.. " moved " ..extra.. " to the NOBODY ASKED vc"
+	killer.. " moved " ..extra.. " to the NOBODY ASKED vc",
+	extra.. " has earned the title: YOU LOST given by " ..killer,
+	extra.. " entered tadc because of " ..killer.. "'s dare",
+	extra.. " got addicted to " ..killer.. "'s gambling machine",
+	killer.. " found out " ..extra.. " was using AI to make code",
+	extra.. " burped and " ..killer.. " sneezed"
 }
