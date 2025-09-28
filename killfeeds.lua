4534d3a323b777227923536ae46ff95f8225ad60
Cametized <cameron.reese.f@gmail.com>
2025-09-28 13:13:12 -0400

    Update killfeeds.lua

--- killfeeds.lua
+++ killfeeds.lua
@@ -3,5 +3,8 @@
 
 local killMessagePool = {
 	extra.." was dishonored by "..killer, -- example
-	extra.. " geeked out on tuesday"
+	extra.. " geeked out on tuesday",
+	killer.. " said a joke and made " ..extra.. " die of laughter",
+	extra.. " dang, you're REALLY bad at this game, lol.",
+	extra.. " ate their billionth banana and died of radiation"
 }
