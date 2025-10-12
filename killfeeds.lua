48f97900882cc8b9c572f85e47373c080167156e
Cametized <cameron.reese.f@gmail.com>
2025-10-12 03:56:36 -0400

    Update killfeeds.lua

--- killfeeds.lua
+++ killfeeds.lua
@@ -1,7 +1,7 @@
 -- # Below, is the entire RS killfeed file, it's a hell, so, like, lock tf in lo
 -- # File checked by @CametizedDev
 
-local killMessagePool = { -- lol
+local killMessagePool = { -- ballin
 	extra.." was dishonored by "..killer, -- example
 	extra.. " geeked out on tuesday",
 	killer.. " said a joke and made " ..extra.. " die of laughter",
